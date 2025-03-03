41cbea6663258e94472ea32f6b135ba21d816815
Ethan Cala <156372497+ethancala@users.noreply.github.com>
2025-03-02 23:30:15 -0600

    Update index.js
    added wake up

--- index.js
+++ index.js
@@ -40,6 +40,12 @@ app.get("/api/roll-dice", (req, res) => {
     res.json({ roll });
 });
 
+// Wake-up endpoint
+app.get("/wake-up", (req, res) => {
+    res.send("Server is awake!");
+});
+
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
