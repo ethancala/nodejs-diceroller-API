082cf601193101e6f55a81823bb33a03bb691e66
Ethan Cala <156372497+ethancala@users.noreply.github.com>
2025-03-02 20:36:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configure CORS (Temporarily allow all origins for debugging)
+// Configure CORS (Block specific origin for failure demonstration)
 // const corsOptions = {
-//     origin: "*",
-//     methods: "GET,POST",
-//     allowedHeaders: "Content-Type",
-// };
+    
+//     origin: function (origin, callback) {
 
+//         //allowed origins
+//         const allowedOrigins = [
+//             "http://localhost:5500",  // Local testing
+//             "http://127.0.0.1:5500",  // Alternative local testing
+//             "https://agreeable-river-09927251e.4.azurestaticapps.net/"
+//         ];
+
+//         if (origin === "https://blocked-origin.com") {
+//             callback(new Error("CORS Error: This origin is blocked"));
+//         } else {
+//             callback(null, true);
+//         }
+//     },
+// };
 //app.use(cors(corsOptions));
 app.use(express.json());
 
